696d30c559372c6cbd6e75d068879bedce15773e
vishal3010 <myemail@example.com>
2024-04-25 14:22:17 +0530

    introduction of cruddata

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,15 +1,14 @@
 import { useState } from "react";
-import { onLogin } from "../api/auth";
 import Layout from "../components/layout";
 import { useDispatch } from "react-redux";
 import { authenticateUser } from "../redux/slices/authSlice";
 import { FaUser, FaLock } from "react-icons/fa";
 import "./login.css";
-import logo from "../components/img/IMG_20240219_090620519_HDR.jpg";
-import Button from "@mui/material/Button";
-import { NavLink, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import crudData from "../config/apiService";
 
 const Login = () => {
+  const dispatch = useDispatch();
   const [values, setValues] = useState({
     email: "",
     password: "",
@@ -20,18 +19,14 @@ const Login = () => {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
 
-  const dispatch = useDispatch();
   const onSubmit = async (e) => {
     e.preventDefault();
-
     try {
-      await onLogin(values);
+      await crudData('/login', 'POST', values, 'authEngine')
       dispatch(authenticateUser());
-
       localStorage.setItem("isAuth", "true");
     } catch (error) {
-      console.log(error.response.data.errors[0].msg);
-      setError(error.response.data.errors[0].msg);
+      setError(error.message);
     }
   };
 
@@ -44,9 +39,6 @@ const Login = () => {
             <h1>Login</h1>
 
             <div className=" input-box">
-              {/* <label htmlFor="email" className="form-label">
-              Email address
-            </label> */}
               <input
                 onChange={(e) => onChange(e)}
                 type="email"
